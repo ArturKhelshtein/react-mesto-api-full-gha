56e84f054233a776ca2df03de09c899921840726
Artur Khelshtein <Khelshtein.a@yandex.ru>
2023-08-23 23:20:21 +0300

    change PORT

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ const { errorMiddleware } = require('./middlewares/errorMiddleware');
 const ErrorNotFound = require('./errors/error-not-found');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
+const { PORT = 4000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000,
